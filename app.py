6c25b27b500934a9c06d71510d89eb102998e9d6
Lê Huy Hoàng <156869304+hoangscor@users.noreply.github.com>
2025-06-10 15:44:57 +0700

    port 0.0.0.0

--- app.py
+++ app.py
@@ -356,5 +356,6 @@ def edit(row_id):
 
 if __name__=='__main__':
     # app.run(host='0.0.0.0', port=8080, debug=True)
-    app.run(host='127.0.0.1', port=5050, debug=True)
+    # app.run(host='127.0.0.1', port=5050, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
 
